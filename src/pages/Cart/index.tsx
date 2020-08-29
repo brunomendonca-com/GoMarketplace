fe61750d1258863aea28f533083f9f3f71000d74
Bruno Mendonca <me@brunomendonca.com>
2020-08-29 10:53:59 -0600

    Add code solution

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -38,24 +38,34 @@ interface Product {
 const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
+  console.log(products);
+
   function handleIncrement(id: string): void {
     // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
     // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productSubTotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return productSubTotal + accumulator;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const quantity = products.reduce((accumulator, product) => {
+      return product.quantity + accumulator;
+    }, 0);
 
-    return 0;
+    return quantity;
   }, [products]);
 
   return (
